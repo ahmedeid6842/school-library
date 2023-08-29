5aeb26be973d971b453562b564e1a63197efdca4
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-29 03:18:31 +0300

    Teacher: Added teacher intializer with it's super class

--- teacher.rb
+++ teacher.rb
@@ -1,4 +1,8 @@
 require_relative "person"
 
 class Teacher < Person
+  def initialize(id, age, specialization, name = "UnKnown", parent_permission = true)
+    super(id, age, name, parent_permission)
+    @specialization = specialization
+  end
 end
