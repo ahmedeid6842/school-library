7735724af23ec2b15f3c80fd55431f8c160e9173
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-29 03:14:07 +0300

    Student: Added the classroom accessor

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative "person"
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(id, age, classroom, name = "Unknown", parent_permission = true)
     super(id, age, name, parent_permission)
     @classroom = classroom
