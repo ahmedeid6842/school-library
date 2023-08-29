bef8b82d11caec60272dfcd09d3f96f6d9ab358b
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-29 03:13:14 +0300

    Student: Added student intializer with it's super class

--- student.rb
+++ student.rb
@@ -1,4 +1,8 @@
 require_relative "person"
 
 class Student < Person
+  def initialize(id, age, classroom, name = "Unknown", parent_permission = true)
+    super(id, age, name, parent_permission)
+    @classroom = classroom
+  end
 end
