34c98931b51fb9a1aac023d7daf65265b326cc47
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-29 03:38:15 +0300

    Fixed linter rubocop errors

--- student.rb
+++ student.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(id, age, classroom, name = 'Unknown', parent_permission = true)
+  def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission)
     @classroom = classroom
   end
--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(id, age, specialization, name = 'UnKnown', parent_permission = true)
+  def initialize(id, age, specialization, name = 'UnKnown', parent_permission: true)
     super(id, age, name, parent_permission)
     @specialization = specialization
   end
