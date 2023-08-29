3478358ad26db715c42086cdb0f6fcb70150fad9
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-29 03:19:15 +0300

    Teacher: Added the specialization accessor

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,8 @@
 require_relative "person"
 
 class Teacher < Person
+  attr_accessor :specialization
+
   def initialize(id, age, specialization, name = "UnKnown", parent_permission = true)
     super(id, age, name, parent_permission)
     @specialization = specialization
