7f510f4264263b5adbdf37b31a8384090edf9060
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-31 23:00:05 +0300

    Student: Adding a student to a classroom requires setting up the classroom for them.

--- student.rb
+++ student.rb
@@ -1,11 +1,12 @@
-require_relative 'person'
+require_relative "person"
 
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(id, age, classroom, name = "Unknown", parent_permission: true)
     super(id, age, name, parent_permission)
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hocky
