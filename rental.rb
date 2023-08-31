6f2d07caade1e1f9515638d17e43f8ddacff0f3a
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-31 23:11:41 +0300

    Created the realtion between rental and book

--- rental.rb
+++ rental.rb
@@ -1,7 +1,10 @@
 class Rental
   attr_accessor :date
+  attr_reader :book
 
-  def initialize(date)
+  def initialize(date, book)
     @date = date
+    @book = book
+    book.rentals << self
   end
 end
