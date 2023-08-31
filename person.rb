851471bb03ecc96b942bb4bcb81bd9e51e00e637
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-31 20:25:25 +0300

    Person: make the id is dynamically generated to fix Nof attributes

--- person.rb
+++ person.rb
@@ -7,8 +7,8 @@ class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(id, age, name = "Unknown", parent_permission: true)
-    @id = id
+  def initialize(age, name = "Unknown", parent_permission: true)
+    @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
