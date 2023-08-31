f0420ac67a60b25cf2def51b942d2311c0cce26f
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-31 20:23:20 +0300

    Person: check if you managed to decorate your person

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 require_relative "nameable"
+require_relative "decorator"
+require_relative "capitalize-decorators"
+require_relative "trimmer-decorator"
 
 class Person < Nameable
   attr_reader :id
@@ -25,3 +28,10 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+person = Person.new(22, "maximilianus")
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
