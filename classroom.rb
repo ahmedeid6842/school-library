ce106579d08dccbcfbc095733f03c2bec7c37d35
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-08-31 22:50:27 +0300

    Classroom: Implement add studnet method for classroom

--- classroom.rb
+++ classroom.rb
@@ -3,5 +3,11 @@ class Classroom
 
   def initialize(label)
     @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    students << student
+    student.classroom = self
   end
 end
