5ec851fb376ddf05e37b27a1598b124330a016cb
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-01 04:04:51 +0300

    Person: removed unneccesasry code

--- person.rb
+++ person.rb
@@ -1,14 +1,14 @@
-require_relative 'nameable'
-require_relative 'decorator'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
-require_relative 'rental'
+require_relative "nameable"
+require_relative "decorator"
+require_relative "capitalize_decorator"
+require_relative "trimmer_decorator"
+require_relative "rental"
 
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age, :rentals
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = "Unknown", parent_permission = true)
     super()
     @id = rand(1..1000)
     @name = name
@@ -35,12 +35,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
