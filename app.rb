b25adff60aebd4f9e89d46389b9e43a1f190d0fa
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-01 03:59:28 +0300

    App: added list people method

--- app.rb
+++ app.rb
@@ -13,4 +13,17 @@ class App
       puts "Title: #{book.title}, Author: #{book.author}"
     end
   end
+
+  def list_people(people)
+    puts "People:"
+    people.each do |person|
+      if person.is_a?(Student)
+        puts "[Studnet] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+      elsif person.is_a?(Teacher)
+        puts "[Teacher] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+      else
+        puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+      end
+    end
+  end
 end
