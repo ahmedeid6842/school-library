701904b0271e79479d578c7c77858c195c31d726
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-01 03:58:53 +0300

    App: added list_books method

--- app.rb
+++ app.rb
@@ -6,4 +6,11 @@ class App
     @people = []
     @rentals = []
   end
+
+  def list_books(books)
+    puts "Books:"
+    books.each do |book|
+      puts "Title: #{book.title}, Author: #{book.author}"
+    end
+  end
 end
