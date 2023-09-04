096bf35147c874417831019a94b29f49e00dfcc3
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-05 02:35:53 +0300

    remove the user inputs from app to main

--- app.rb
+++ app.rb
@@ -8,14 +8,14 @@ class App
   end
 
   def list_books(books)
-    puts 'Books:'
+    puts "Books:"
     books.each do |book|
       puts "Title: #{book.title}, Author: #{book.author}"
     end
   end
 
   def list_people(people)
-    puts 'People:'
+    puts "People:"
     people.each do |person|
       if person.is_a?(Student)
         puts "[Studnet] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
@@ -30,7 +30,7 @@ class App
   def create_studnet(age, name, classroom_label, permission_input)
     classroom = Classroom.new(classroom_label)
 
-    permission = permission_input == 'y'
+    permission = permission_input == "y"
 
     person = Student.new(age, classroom, name, parent_permission: permission)
 
@@ -45,84 +45,14 @@ class App
     puts "Person created with ID: #{person.id}"
   end
 
-  def create_person(kind, _app)
-    puts "Enter the person's name:"
-    name = gets.chomp
-    puts "Enter the person's age:"
-    age = gets.chomp.to_i
-
-    case kind
-    when 1
-      puts "Enter the student's classroom label:"
-      classroom_label = gets.chomp
-
-      puts 'Has parent permission? [Y/N]:'
-      permission_input = gets.chomp.downcase
-
-      create_studnet(age, name, classroom_label, permission_input)
-    when 2
-      puts "Enter the teacher's specialization:"
-      specialization = gets.chomp
-      create_teacher(age, name, specialization)
-    else
-      puts 'Invalid input.'
-      nil
-    end
-  end
-
-  def create_book(books)
-    puts "Enter the book's title:"
-    title = gets.chomp
-    puts "Enter the book's author:"
-    author = gets.chomp
-
+  def create_book(books, title, author)
     book = Book.new(title, author)
     books << book
 
     puts "Book created with title: #{book.title}"
   end
 
-  def list_people_rental(people)
-    puts 'Select a person from the following list by number:'
-    people.each_with_index do |person, index|
-      if person.is_a?(Student)
-        puts "#{index}) [Student] Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
-      elsif person.is_a?(Teacher)
-        puts "#{index}) [Teacher] Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
-      else
-        puts "#{index}) Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
-      end
-    end
-  end
-
-  def list_books_rental(books)
-    puts 'Select a book from the book list number:'
-    books.each_with_index do |book, index|
-      puts "#{index}) Title: #{book.title}, Author: #{book.author}"
-    end
-  end
-
-  def create_rental(books, people, rentals)
-    list_books_rental(books)
-
-    book_number = gets.chomp.to_i
-    book = books[book_number]
-    unless book
-      puts 'Invalid book number.'
-      return
-    end
-
-    list_people_rental(people)
-    person_number = gets.chomp.to_i
-    person = people[person_number]
-    unless person
-      puts 'Invalid person number.'
-      return
-    end
-
-    puts 'Enter the rental date (YYYY-MM-DD):'
-    date = gets.chomp
-
+  def create_rental(date, book, person)
     rental = Rental.new(date, book, person)
     rentals << rental
 
@@ -134,7 +64,7 @@ class App
     person_id = gets.chomp.to_i
     person = people.find { |p| p.id == person_id }
     unless person
-      puts 'Person not found.'
+      puts "Person not found."
       return
     end
 
--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
-require_relative 'app'
-require_relative 'book'
-require_relative 'person'
-require_relative 'student'
-require_relative 'teacher'
-require_relative 'rental'
-require_relative 'classroom'
+require_relative "app"
+require_relative "book"
+require_relative "person"
+require_relative "student"
+require_relative "teacher"
+require_relative "rental"
+require_relative "classroom"
 
 MENU_OPTIONS = {
   1 => :list_books,
@@ -13,7 +13,7 @@ MENU_OPTIONS = {
   4 => :create_book,
   5 => :create_rental,
   6 => :list_rentals_for_person,
-  7 => :exit
+  7 => :exit,
 }.freeze
 
 def main
@@ -29,22 +29,22 @@ def main
 
       send(MENU_OPTIONS[choice], app)
     else
-      puts 'Invalid choice.'
+      puts "Invalid choice."
     end
 
-    puts ''
+    puts ""
   end
 end
 
 def display_menu_options
-  puts 'What would you like to do?'
-  puts '1. List all books'
-  puts '2. List all people'
-  puts '3. Create a person'
-  puts '4. Create a book'
-  puts '5. Create a rental'
-  puts '6. List rentals for a person'
-  puts '7. Exit'
+  puts "What would you like to do?"
+  puts "1. List all books"
+  puts "2. List all people"
+  puts "3. Create a person"
+  puts "4. Create a book"
+  puts "5. Create a rental"
+  puts "6. List rentals for a person"
+  puts "7. Exit"
 end
 
 def list_books(app)
@@ -56,17 +56,81 @@ def list_people(app)
 end
 
 def create_person(app)
-  puts 'Enter the kind of person (1 for student, 2 for teacher):'
+  puts "Enter the kind of person (1 for student, 2 for teacher):"
   kind = gets.chomp.to_i
-  app.create_person(kind, app)
+  puts "Enter the person's name:"
+  name = gets.chomp
+  puts "Enter the person's age:"
+  age = gets.chomp.to_i
+
+  case kind
+  when 1
+    puts "Enter the student's classroom label:"
+    classroom_label = gets.chomp
+
+    puts "Has parent permission? [Y/N]:"
+    permission_input = gets.chomp.downcase
+    app.create_studnet(age, name, classroom_label, permission_input)
+  when 2
+    puts "Enter the teacher's specialization:"
+    specialization = gets.chomp
+    app.create_teacher(age, name, specialization)
+  else
+    puts "Invalid input."
+    nil
+  end
 end
 
 def create_book(app)
-  app.create_book(app.books)
+  puts "Enter the book's title:"
+  title = gets.chomp
+  puts "Enter the book's author:"
+  author = gets.chomp
+  app.create_book(app.books, title, author)
+end
+
+def list_books_rental(books)
+  puts "Select a book from the book list number:"
+  books.each_with_index do |book, index|
+    puts "#{index}) Title: #{book.title}, Author: #{book.author}"
+  end
+end
+
+def list_people_rental(people)
+  puts "Select a person from the following list by number:"
+  people.each_with_index do |person, index|
+    if person.is_a?(Student)
+      puts "#{index}) [Student] Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
+    elsif person.is_a?(Teacher)
+      puts "#{index}) [Teacher] Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
+    else
+      puts "#{index}) Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
+    end
+  end
 end
 
 def create_rental(app)
-  app.create_rental(app.books, app.people, app.rentals)
+  list_books_rental(app.books)
+
+  book_number = gets.chomp.to_i
+  book = app.books[book_number]
+  unless book
+    puts "Invalid book number."
+    return
+  end
+
+  list_people_rental(app.people)
+  person_number = gets.chomp.to_i
+  person = app.people[person_number]
+  unless person
+    puts "Invalid person number."
+    return
+  end
+
+  puts "Enter the rental date (YYYY-MM-DD):"
+  date = gets.chomp
+
+  app.create_rental(date, book, person)
 end
 
 def list_rentals_for_person(app)
